e2c80decdbffda08e30ebb3d80a8cae378ac86ca
tiger <1220289215@qq.com>
2020-11-11 17:41:58 +0800

    1.协程的上下文和分发器

--- app/src/main/java/com/lxl/essence/coroutines/Coroutine.kt
+++ app/src/main/java/com/lxl/essence/coroutines/Coroutine.kt
@@ -372,11 +372,34 @@ class Compose {
 }
 
 
-fun main() {
+fun compose() {
     val compose = Compose();
-//    compose.sequential()
-//    compose.concurrent()
-//    compose.lazyConcurrent()
+    compose.sequential()
+    compose.concurrent()
+    compose.lazyConcurrent()
     compose.safeConcurrent()
+}
+
+class Dispatcher{
+    fun simpleContext()= runBlocking {
+
+        launch { // context of the parent, main runBlocking coroutine
+            println("main runBlocking      : I'm working in thread ${Thread.currentThread().name}")
+        }
+        launch(Dispatchers.Unconfined) { // not confined -- will work with main thread
+            println("Unconfined            : I'm working in thread ${Thread.currentThread().name}")
+        }
+        launch(Dispatchers.Default) { // will get dispatched to DefaultDispatcher
+            println("Default               : I'm working in thread ${Thread.currentThread().name}")
+        }
+        launch(newSingleThreadContext("MyOwnThread")) { // will get its own new thread
+            println("newSingleThreadContext: I'm working in thread ${Thread.currentThread().name}")
+        }
+    }
+}
+
+fun main() {
+    val dispatcher =Dispatcher();
+    dispatcher.simpleContext()
 }
 
